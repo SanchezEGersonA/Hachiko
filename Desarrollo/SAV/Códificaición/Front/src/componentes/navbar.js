07aabaca717e8d84d44db4c9ab5e61b1bb0b3bd8
EstefanoAguirre <estefano.aguirre@unmsm.edu.pe>
2019-07-03 10:08:05 -0500

    swat en navbar

--- Desarrollo/SAV/Códificaición/Front/src/componentes/navbar.js
+++ Desarrollo/SAV/Códificaición/Front/src/componentes/navbar.js
@@ -13,7 +13,27 @@ class navbar extends React.Component{
     }
 
     Cerrar=(e)=>{
-        swal("" ,"", "success").then(browserHistory.push('/'))
+        //swal("" ,"", "success").then(browserHistory.push('/'))
+
+        swal({
+            title: "¿Está seguro que quiere cerrar la sesión?",
+            text: "perdera los cambios sin guardar",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+              swal("sesión cerrada exitosamente...!", {
+                icon: "success",
+              }).then(
+                browserHistory.push('/')
+              )
+              ;
+            } else {
+              swal("Guarda los cambios antes de salir");
+            }
+          });
 
     }
 
